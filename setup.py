59dc9c405ef6bcd40f52a66f8e8c6e642bde17f7
Yunjae Lee <lyj7694@gmail.com>
2020-08-25 17:08:42 +0900

    Fix obsolete project names
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-        name = "edms-yunjae2",
+        name = "edm-yunjae2",
         version = "0.0.1",
         author = "Yunjae Lee",
         author_email = "lyj7694@gmail.com",
         description = "Experiment data management system",
         long_description = long_description,
         long_description_content_type = "text/markdown",
-        url = "https://github.com/yunjae2/edms",
+        url = "https://github.com/yunjae2/edm",
         packages = setuptools.find_packages(),
         classifiers = [
             "Programming Language :: Python :: 3",
