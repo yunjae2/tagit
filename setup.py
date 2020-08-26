01937dd65fa8a6cf4a4ad4c3bf26f360ff2ea836
Yunjae Lee <lyj7694@gmail.com>
2020-08-26 18:38:21 +0900

    Release v0.0.2
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
         name = "etagit",
-        version = "0.0.1",
+        version = "0.0.2",
         author = "Yunjae Lee",
         author_email = "lyj7694@gmail.com",
         description = "CLI tagging system for experiment data",
